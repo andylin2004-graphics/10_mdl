10c17b4bc8793550333244ba14074c7ef7c47777
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-05-16 22:16:31 -0400

    converted main to handle the file stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+
+use lrlex::lrlex_mod;
+use lrpar::lrpar_mod;
+
+//tell lexer which lex file to use
+lrlex_mod!("mdl.l");
+//tell parser which parser to use
+lrpar_mod!("mdl.y");
+
 fn main() {
-    println!("Hello, world!");
+    let lexerdef = mdl_l::lexerdef();
+    let args: Vec<String> = env::args().collect();
+    let lexer = lexerdef.lexer(&args[1]);
+    let (res, errs) = mdl_y::parse(&lexer);
+    for e in errs{
+        println!("{}", e.pp(&lexer, &mdl_y::token_epp));
+    }
+    match res{
+        Some(r) => println!("Result: {:?}", r),
+        _ => eprintln!("Unable to evaluate expression.")
+    }
 }
