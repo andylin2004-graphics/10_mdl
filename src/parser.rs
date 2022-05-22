6e0870cbe40ce527301cb453507e0b4fe8e510b5
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-05-22 13:43:14 -0400

    added rule for constants with 9 ins

--- src/parser.rs
+++ src/parser.rs
@@ -3,21 +3,36 @@ use crate::image::Image;
 use crate::matrix::CurveType;
 use crate::matrix::Matrix;
 use crate::pest::{iterators::Pair, Parser};
+use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
-use std::io::{self, BufReader, BufRead, Read};
+use std::io::{self, BufRead, BufReader, Read};
 
 #[derive(Parser)]
 #[grammar = "mdl.pest"]
 struct MDLParser;
 
-struct Constants{
+#[derive(Debug)]
+struct Constants {
     pub r: [f32; 3],
     pub g: [f32; 3],
     pub b: [f32; 3],
     pub red: f32,
     pub green: f32,
-    pub blue: f32
+    pub blue: f32,
+}
+
+impl Constants {
+    fn new() -> Constants {
+        Constants {
+            r: [0.0, 0.0, 0.0],
+            g: [0.0, 0.0, 0.0],
+            b: [0.0, 0.0, 0.0],
+            red: 0.0,
+            green: 0.0,
+            blue: 0.0,
+        }
+    }
 }
 
 pub fn parse(fname: &str) -> io::Result<()> {
@@ -31,10 +46,39 @@ pub fn parse(fname: &str) -> io::Result<()> {
     let mut edges = Matrix::new(0, 0);
     let mut polygons = Matrix::new(0, 0);
     let mut cstack = vec![Matrix::new(0, 0); 0];
+    let mut constants_store = HashMap::new();
     for pair in commands {
         for command in pair {
-            match command.as_rule(){
-                // Rule::
+            println!("{:?}", command.as_rule());
+            match command.as_rule() {
+                Rule::CONSTANTS_SDDDDDDDDD => {
+                    let mut command_contents = command.into_inner();
+                    let name = command_contents.next().unwrap().as_str();
+                    println!("{:?}", command_contents);
+                    let constant = Constants {
+                        r: [
+                            command_contents.next().unwrap().as_str().parse().unwrap(),
+                            command_contents.next().unwrap().as_str().parse().unwrap(),
+                            command_contents.next().unwrap().as_str().parse().unwrap(),
+                        ],
+                        g: [
+                            command_contents.next().unwrap().as_str().parse().unwrap(),
+                            command_contents.next().unwrap().as_str().parse().unwrap(),
+                            command_contents.next().unwrap().as_str().parse().unwrap(),
+                        ],
+                        b: [
+                            command_contents.next().unwrap().as_str().parse().unwrap(),
+                            command_contents.next().unwrap().as_str().parse().unwrap(),
+                            command_contents.next().unwrap().as_str().parse().unwrap(),
+                        ],
+                        red: 0.0,
+                        green: 0.0,
+                        blue: 0.0,
+                    };
+                    constants_store.insert(name, constant);
+                    // println!("{:?}", constant);
+                }
+                _ => {}
             }
         }
     }
