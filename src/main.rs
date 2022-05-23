6ee04cc18096867d8348ab4c8175ba896a13b346
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-05-23 09:33:33 -0400

    can choose to make art or the assignment default

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,15 @@ use matrix::CurveType;
 use matrix::Matrix;
 use parser::parse;
 use reflect::ReflectionValue;
+use std::env;
 
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-pub mod consts{
+pub mod consts {
     use crate::color::Color;
-    use crate::reflect::ReflectionValue; 
+    use crate::reflect::ReflectionValue;
 
     pub const AMBIENT_COLOR: Color = Color::new_color(50, 50, 50);
     pub const AMBIENT_REFLECT: ReflectionValue = ReflectionValue::new_values(0.1, 0.1, 0.1);
@@ -26,11 +27,16 @@ pub mod consts{
     pub const SPECULAR_REFLECT: ReflectionValue = ReflectionValue::new_values(0.5, 0.5, 0.5);
     pub const POINT_LIGHT_LOCATION: [f32; 3] = [0.5, 0.75, 1.0];
     pub const POINT_LIGHT_COLOR: Color = Color::new_color(255, 255, 255);
-    pub const VIEW: [f32; 3] = [0.0,0.0,1.0];
+    pub const VIEW: [f32; 3] = [0.0, 0.0, 1.0];
     pub const STEP_2D: i32 = 100;
     pub const STEP_3D: i32 = 100;
 }
 
 fn main() {
-    parse("coloredbot.mdl");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 && args[1] == "art"{
+        parse("coloredbot.mdl");
+    }else{
+        parse("face.mdl");
+    }
 }
